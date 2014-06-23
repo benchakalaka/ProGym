521d18c7560c25fe35012798c6c4fc34bfff117a
Ihor Karpachev <benchakalaka@gmail.com>
2014-06-23 16:31:27 +0100

    cleaning repo

--- src/com/progym/utils/AppSharedPreferences.java
+++
@@ -1,7 +0,0 @@
-package com.progym.utils;
-
-import org.androidannotations.annotations.sharedpreferences.SharedPref;
-
-@SharedPref ( value = SharedPref.Scope.UNIQUE ) public interface AppSharedPreferences {
-
-}
